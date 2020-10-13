118200fe51c0a28767b73abe0fe1e1264dd6e5ad
Utkarsh Pandey <utkarsh.pandey2710@gmail.com>
2020-10-13 16:58:36 +0530

    redux functionality

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -23,7 +23,7 @@ const orderSchema = mongoose.Schema(
     shippingAddress: {
       address: { type: String, required: true },
       city: { type: String, required: true },
-      postalcode: { type: String, required: true },
+      postalCode: { type: String, required: true },
       country: { type: String, required: true },
     },
     paymentMethod: {
@@ -69,7 +69,7 @@ const orderSchema = mongoose.Schema(
     },
   },
   {
-    timeStamps: true,
+    timestamps: true,
   }
 );
 
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from "express";
-
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -16,6 +16,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
